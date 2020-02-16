d9b0e825c945b2dc8476197e9c9b4f8b7f7d28f1
Alex Tilatti <alex.tilatti@gmail.com>
2020-02-16 18:13:53 -0500

    added the name editing feature and fixed the delete button

--- src/App.js
+++ src/App.js
@@ -5,38 +5,67 @@ import Person from "./components/Person"
 function App(props) {
   
   const [data, setData] = useState({
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 30},
-      {name: 'Steph', age: 23}
+    persons:  [
+      {id: 1, name: 'Max', age: 28},
+      {id: 2, name: 'Manu', age: 30},
+      {id: 3, name: 'Steph', age: 23}
     ]
-  })  ;
+  });
 
-  const switchNameHandler = () => {
-    setData({
-      persons: [
-        {name: 'Maxwell', age: 28},
-        {name: 'Manu Ginobili', age: 30},
-        {name: 'Steph', age: 23}
-      ]
+  const [toggle, setToggle] = useState(false);
+
+  const switchNameHandler = (event, id) => {
+
+    const findPerson = data.persons.findIndex((person) => {
+      return person.id === id;
+    });
+
+    let newPerson = { ...data.persons[findPerson] }; 
+    // let newperson = { ...data.persons[findPerson] };
+    newPerson.name = event.target.value;
+    console.log(newPerson.name);
+    data.persons[findPerson] = newPerson;
+    setData(prevState => {
+      return {...prevState, ...data}
     });
   };
+
+  const switchToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const deletePerson = (index) => {
+    data.persons.splice(index, 1);
+    setData( prevState => {
+      return {...prevState, ...data}
+    });
+  };
+
+  let persons = null;
+
+  if (toggle) {
+    persons = (
+      <div>
+        {data.persons.map((person, index) => {
+          return <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              click={ () => deletePerson(index)}
+              changed={ (event) => switchNameHandler(event, person.id)}
+            />
+          })  
+        }
+      </div>
+    )
+  }
 
   return (
     <div className="App">
       <h1>Hi I am a react app</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person
-        name={data.persons[0].name}
-        age={data.persons[0].age}
-      />
-      <Person
-        name={data.persons[1].name}
-        age={data.persons[1].age}> My hobbies are: Soccer </Person>
-      <Person
-        name={data.persons[2].name}
-        age={data.persons[2].age}
-      />
+      {/* <button onClick={switchNameHandler}>Switch Name</button> */}
+      <button onClick={switchToggle}>Toggle Peeps</button>
+      {persons}
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -5,9 +5,9 @@ import './Person.css';
 const person = props => {
   return (
     <div className="person">
-      {/* <button>Switch Name</button> */}
       <p> I'm {props.name} and I'm {props.age} years old </p>
-      <p> {props.children} </p>
+      <input onChange={props.changed} type='text' />
+      <button onClick={props.click}> Delete Person </button>
     </div>
   )
 }
